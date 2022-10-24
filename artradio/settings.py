7ac761951ed800e8d1c89e5a119a551c09e7a7fb
devraider <56563291+devraider@users.noreply.github.com>
2022-10-24 22:25:30 +0300

    Removed dotenv

--- artradio/settings.py
+++ artradio/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import yaml
-from dotenv import load_dotenv
-load_dotenv("./.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
