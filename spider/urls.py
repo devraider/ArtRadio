2e0f9c6f2ffdc1d30532b0e0059878b31c5c386c
devraider <56563291+devraider@users.noreply.github.com>
2022-10-28 00:28:01 +0300

    Added Shaggi logic

--- spider/urls.py
+++ spider/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('wlc', views.spider_welcome, name="welcome_spider"),
     path('impuls', views.spider_radio, name="spider_radio"),
-    path('yt_search', views.yt_spider_search, name="yt_spider_search")
+    path('yt_search', views.yt_spider_search, name="yt_spider_search"),
+    path('yt_download', views.yt_download, name="yt_download")
 
 ]
